625d7a0c361dd66f21d87a57aacd1f42ef02d601
juliakent77 <juliakent@Julias-MacBook-Air.local>
2016-12-20 15:51:31 -0800

    jQuery conversion take 1
    I definitely broke it…

---
+++ script.js
@@ -0,0 +1,39 @@
+
+// var todoForm = document.getElementById('todo-form');
+// var todoList = document.getElementById('todo-list');
+
+
+var todoForm = $('todo-form');
+var todoList = $('todo-list');
+
+$('submit').click(function(event) {
+  event.preventDefault();
+  var elem = $(event.target);
+
+  var todo = elem.todoInput.value;
+
+  var li = $(add.('li'));
+  var input = $(add.('input'));
+  var p = $(add.('p'));
+  var deleteButton = $(add.('button'));
+  var clearDiv = $(add.('div'));
+
+  li.className = 'fadeIn';
+  input.type = 'checkbox';
+  p.innerHTML = todo;
+  deleteButton.innerHTML = 'Delete';
+  clearDiv.className = 'clearfix';
+
+  deleteButton.addEventListener('click', function(event) {
+    elem.parentNode.remove();
+  });
+
+  $('li').append(input);
+  $('li').append(p);
+  $('li').append(deleteButton);
+  $('li').append(clearDiv);
+
+  $('todoList').append(li);
+
+  elem.todoInput.value = '';
+});
